5f8e85fdb0c6eeeee76f3cd19f2f84bf91c170dc
evening0twilight <980898953@qq.com>
2025-08-13 14:35:07 +0800

    feat:部分修改尝试提交

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(dto: { username: string; password: string }) {
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+    return this.usersService.create({
+      ...dto,
+      password: hashedPassword,
+    });
+  }
+}
