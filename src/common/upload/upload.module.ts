7bc8f9b79d18042aff7e9decfe72c84dd9f30d73
evening0twilight <980898953@qq.com>
2025-10-13 19:30:58 +0800

    fix:修复上传头像路径和token关键词错误的问题

--- src/common/upload/upload.module.ts
+++ src/common/upload/upload.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 import cosConfig from './cos.config';
 
 @Module({
-  imports: [ConfigModule.forFeature(cosConfig)],
+  imports: [
+    ConfigModule.forFeature(cosConfig),
+    PassportModule,
+    JwtModule.register({}), // 注册 JWT 模块以供 JwtAuthGuard 使用
+  ],
   controllers: [UploadController],
   providers: [UploadService],
   exports: [UploadService],
