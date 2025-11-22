91e919fc787bec6e140446243cae1a313a5cd02b
evening0twilight <980898953@qq.com>
2025-11-22 15:57:58 +0800

    fix: 修复 UploadModule 依赖注入错误

--- src/common/upload/upload.module.ts
+++ src/common/upload/upload.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@/users/user.entity';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 import cosConfig from './cos.config';
@@ -11,6 +13,7 @@ import cosConfig from './cos.config';
     ConfigModule.forFeature(cosConfig),
     PassportModule,
     JwtModule.register({}), // 注册 JWT 模块以供 JwtAuthGuard 使用
+    TypeOrmModule.forFeature([User]), // 导入 User 实体以提供 UserRepository
   ],
   controllers: [UploadController],
   providers: [UploadService],
